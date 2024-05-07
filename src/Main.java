95d1cb7be38ac25dab7ec927a268ea3f12e9962b
cirilo <joseciriloguzmangalindo@gmail.com>
2024-05-06 18:08:54 -0600

    Added find and bucket find method

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,23 @@ public class Main {
         Error error = new Error();
 
         int mainMenuOption;
-        String menuText= "Please, choose an option\n[1]- CREATE REGISTRY\n[2]- PRINT ALL\n[3]- ADD\n[4]- PRINT ACTIVE ENTRIES\n[5]- SEARCH\n[9]- EXIT";
+        String menuText= "Please, choose an option\n[1]- ADD\n[2]- PRINT ACTIVE ENTRIES\n[3]- SEARCH\n[8]- DELETE ALL\n[9]- EXIT";
 
         do {
             data=new Data(false,0,"","",0.0f,true,0);
             mainMenuOption = input.readInt(menuText);
             switch (mainMenuOption) {
                 case 1 ->{
-                    file.initialize();
-                    System.out.println("File created");
-                    System.out.println("\n");
+                    data.request();
                 }
                 case 2 -> {
                     File fileName=new File("data.dat");
+                    file.printAllEntries(fileName);
+                    fileName=new File("bucket.dat");
+                    file.printAllEntries(fileName);
+
+                    //old print all
+                    /*File fileName=new File("data.dat");
                     if(fileName.exists()){
                         file.printAll(fileName);
                     }
@@ -30,19 +34,10 @@ public class Main {
                     if(fileName.exists()){
                         file.printAll(fileName);
                     }
-                    else error.print("Error! empty bucket");
+                    else error.print("Error! empty bucket");*/
 
                 }
                 case 3 -> {
-                    data.request();
-                }
-                case 4 -> {
-                    File fileName=new File("data.dat");
-                    file.printAllEntries(fileName);
-                    fileName=new File("bucket.dat");
-                    file.printAllEntries(fileName);
-                }
-                case 5-> {
                     long overflow;
                     int idToFind = input.readInt("Please, input the value you want to find");
                     File fileName = new File("data.dat");
@@ -54,6 +49,28 @@ public class Main {
                     else{
                         error.print("No entries active in "+fileName.getName());
                     }
+                }
+                case 8->{
+                    int opc=0;
+                    do {
+                        opc = input.readInt("Are you sure you want to delete all entries?\n[1] - Yes,delete all\t\t[2] - Cancel");
+
+                        switch (opc) {
+                            case 1-> {
+                                file.initialize();
+                                System.out.println("All entries deleted");
+                                System.out.println("\n");
+                            }
+
+                            case 2-> {
+                                System.out.println("Operation canceled");
+                            }
+
+                            default -> {
+                                error.print("Invalid option! Try again");
+                            }
+                        }
+                    }while (opc<1||opc>2);
                 }
                 case 9->{}
 
